a03dcccb811ce32a913af92ad73645fc75faf980
araceliaragon1 <42850409+araceliaragon1@users.noreply.github.com>
2018-09-14 23:42:24 -0400

    Add files via upload

--- problem4.java
+++ problem4.java
@@ -1,15 +1,31 @@
-package lab1;
-
 import java.util.Scanner;
 
 public class problem4 {
+	
+	private static int character1;
+	private static int character2; 
+	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the mass in kilograms: ");
-		double mass = input.nextDouble();
-		double speedOfLight = 28.3495 * mass;
-			System.out.println("The energy is: " + speedOfLight + " when the mass is " + mass);
-
+		Scanner input = new Scanner (System.in);
+		System.out.println("Give me a character: ");
+		character1 = input.nextInt();
+		System.out.println("Give me a second character: ");
+		character2 = input.nextInt();
+	
+		for (char i = 0; i < character1.length(); i++)
+		{
+			char character1 = character1.char(i);
+			int value = character1;
+			System.out.println(value);
+		}
+		
+		for (char i = 0; i < character2.length(); i++)
+		{
+			char character2 = character2.char(i);
+			int value = character2;
+			System.out.println(value);
+		}
+		
 	}
 
 }
---
+++ problem6.java
@@ -0,0 +1,9 @@
+
+public class problem6 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("("+ Math.random() + Math.random() + Math.random() + ")" + Math.random() + Math.random + Math.random() + "-" + Math.random() + Math.random() + Math.random() + Math.random());
+	}
+
+}
